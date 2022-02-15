e4c0d75bc418b891f79c00878a381cac3ad0964c
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-02-15 09:51:22 -0800

    Delimited files

--- deliminatedFile/main.cpp
+++ deliminatedFile/main.cpp
@@ -7,13 +7,22 @@ int main() {
     ifstream in;
     ofstream out("outDelim.txt");
     in.open("inDelim.txt");
-    string temp = "\t";
+    string temp;
+    string name;
+    char ch;
+    int i;
     
     if (!!in.is_open()) {
         cout << "File not found\n";
     }
     
     while (getline(in, temp, ',')) {
-        
+        getline(in, temp, ',');
+        ch = temp.at(0);
+        getline(in, temp);
+        i = stoi(temp);
+        out << name << '\t' << ch << '\t' << i << '\n';
     }
+    in.close();
+    out.close();
 }
